bda534b5588e78d0b40c20fe9680cb18f659b1f8
Gerald <geraldmichaelaustria@gmail.com>
2016-08-17 02:11:28 +0800

    User Profile with modal filter

--- frontend/js/user.js
+++ frontend/js/user.js
@@ -1,4 +1,9 @@
-$('.post-card p').each(function(index){
-	var len = $(this).text();
-	$(this).text($(this).text().substring(0,200) + '...');
+$(document).ready(function(){
+	$('.post-card p').each(function(index){
+		var len = $(this).text().length;
+		if(len >= 120){
+			$(this).text($(this).text().substring(0,120) + '...');
+		}
+	});
+	$('.mdb-select').material_select();
 });
